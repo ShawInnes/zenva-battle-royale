3037cdfa81fc866aedd6159d6e762a7736bc4993
Shaw Innes <shaw@immortal.net.au>
2020-01-12 15:50:33 +1000

    Basic Camera Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Look Sensitivity")]
+    public float sensX;
+    public float sensY;
+
+    [Header("Clamping")]
+    public float minY;
+    public float maxY;
+
+    [Header("Spectator")]
+    public float spectatorMoveSpeed;
+
+    private float rotX;
+    private float rotY;
+
+    private bool isSpectator = true;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        // get the mouse movement inputs
+        rotX += Input.GetAxis("Mouse X") * sensX;
+        rotY += Input.GetAxis("Mouse Y") * sensY;
+
+        // limit vertical rotation
+        rotY = Mathf.Clamp(rotY, minY, maxY);
+
+        if (isSpectator)
+        {
+            // rotate the camera vertically
+            transform.rotation = Quaternion.Euler(-rotY, rotX, 0);
+
+            // movement
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            float y = 0;
+
+            if (Input.GetKey(KeyCode.E))
+                y = 1;
+            else if (Input.GetKey(KeyCode.Q))
+                y = -1;
+
+            Vector3 direction = transform.right * x + transform.up * y + transform.forward * z;
+            transform.position += direction * spectatorMoveSpeed * Time.deltaTime;
+        }
+    }
+}
